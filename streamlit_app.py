8139b2aa134cd047e19ef44cc0925875b909c475
tavishi2004 <105282768+tavishi2004@users.noreply.github.com>
2023-07-24 13:44:22 +0530

    Update streamlit_app.py
    adding text box for search

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 # Display the fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # New sectuion to display Fruityvice Api Response
 import requests
