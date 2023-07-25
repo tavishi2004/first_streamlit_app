1fb85989fe6f31d99e45c7ad40ce8d870c530315
tavishi2004 <105282768+tavishi2004@users.noreply.github.com>
2023-07-25 13:48:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
-import streamlit
+import streamlit      
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale , Spinach & Rocket Smoothie')
